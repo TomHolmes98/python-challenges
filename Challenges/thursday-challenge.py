62a69011e42572affafcece3497a4761e3a325e7
Tom Holmes <twjholmes98@gmail.com>
2021-04-30 09:01:41 +0100

    Challenge attempted

--- Challenges/thursday-challenge.py
+++ Challenges/thursday-challenge.py
@@ -1,7 +1,7 @@
 def chall(phrase):
-    phrase_splitted = phrase.split(' ')
+    split_phrase = phrase.split(' ')
     word_list = []
-    for i in phrase_splitted:
+    for i in split_phrase:
         if i not in word_list:
             word_list.append(i)
         else:
@@ -10,6 +10,7 @@ def chall(phrase):
     return((' ').join(word_list))
 
 print(chall('hello world and practice makes perfect and hello world again'))
+
 
 
 
